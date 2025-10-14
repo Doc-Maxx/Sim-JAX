5505638fca3e5e646cc4d4f635a03e7933c9d4ac
Doc-Maxx <maxx.miller95@gmail.com>
2025-10-14 15:31:33 -0700

    Added many comments to the code

--- demo/two_plate_capacitor.py
+++ demo/two_plate_capacitor.py
@@ -7,7 +7,7 @@ Created on Tue Oct 14 07:04:18 2025
 """
 
 '''
-This code is inspired by some of Zhengtao Gan's online CFD course.
+This code reuses some code from Zhengtao Gan's online CFD course.
 
 The physical setup is from N. Giordano & H. Nakanishi, Computational Physics 2nd Ed.
 '''
@@ -39,14 +39,17 @@ def laplace2d_jax2_cap_plate(V,dx,dy,BX, BY, l1norm_target):
     def stepper(V, dx, dy, BX, BY):
         #steps Laplace solver :)
         Vn = V.copy()
+        
+        # Finite difference method to compute the updated voltage within each cell
         V = V.at[1:-1,1:-1].set(((dy**2 * (Vn[2:,1:-1] + Vn[:-2,1:-1]) +
                         dx**2 * (Vn[1:-1,2:] + Vn[1:-1,:-2])) /
                         (2 * (dx**2 + dy**2))))
         
-        #jax.debug.print("{V}",V=V)
+        # Enforce plate voltages 
         V= jax.lax.dynamic_update_slice(V,plate1, (BX, BY))
         V= jax.lax.dynamic_update_slice(V,plate2, (BX,NY- BY))
-        #jax.debug.print("{V}",V=V)
+        
+        # Enforce Boundary Conditions
         V = V.at[0,:].set(0)  
         V = V.at[-1,:].set(0)
         V = V.at[:,0].set(0)  
@@ -55,6 +58,7 @@ def laplace2d_jax2_cap_plate(V,dx,dy,BX, BY, l1norm_target):
         V = V.at[:,-2].set(V[:,-1])
         V = V.at[1,:].set(V[0,:])  
         V = V.at[-2,:].set(V[-1,:])
+        
         return V, Vn
     
     def condition(loop_state):
@@ -63,7 +67,12 @@ def laplace2d_jax2_cap_plate(V,dx,dy,BX, BY, l1norm_target):
         Vn = loop_state[1]
         l1norm_target = loop_state[-1]
         
-        #Compute Condition
+        ''' 
+        Compute "norm" to compare the new voltage matrix to the previous one
+        This solver uses a relaxational method. Each step makes finer adjustments
+        to the voltage distribution. Once we reach an abritrarily small difference
+        between each step, we send the signal to terminate the loop.
+        '''
         norm = (jnp.sum(jnp.abs(V[:])-jnp.abs(Vn[:])) / jnp.sum(jnp.abs(Vn[:])))
         return norm > l1norm_target
     
@@ -75,6 +84,7 @@ def laplace2d_jax2_cap_plate(V,dx,dy,BX, BY, l1norm_target):
         BX = loop_state[4]
         BY = loop_state[5]
         
+        # Solve Step
         V, Vn = stepper(V, dx, dy, BX, BY)
         
         #Repack Loop State
@@ -89,13 +99,19 @@ def laplace2d_jax2_cap_plate(V,dx,dy,BX, BY, l1norm_target):
 # Set up simulation parameters
 NX = 110
 NY = 101
+
+L1NORM_TARGET = 1e-6
+
 dx = 2 / (NX - 1)
 dy = 2 / (NY - 1)
 
-V = jnp.zeros((NX,NY))
-
 x = jnp.linspace(-10,10,NX)
 y = jnp.linspace(-10,10,NY)
+
+# Set up initial conditions for the voltage
+V = jnp.zeros((NX,NY))
+
+# Set Boundary conditions
 V = V.at[0,:].set(0)  
 V = V.at[-1,:].set(0)
 V = V.at[:,0].set(0)  
@@ -105,15 +121,20 @@ V = V.at[:,-2].set(V[:,-1])
 V = V.at[1,:].set(V[0,:])  
 V = V.at[-2,:].set(V[-1,:])
 
+# BX and BY set the location of each plate
 BX = jnp.int32(NX/3)
 BY = jnp.int32(NY/3)
 
+# Set plate voltages to 1 and -1.
+# Each plate is a vector we will slice into the initial voltage matrix
 plate1 = jnp.ones((NX-2*BX,1))
 plate2 = jnp.ones((NX-2*BX,1))*(-1)
 
+# Slice the plates into the voltage matrix
 V= jax.lax.dynamic_update_slice(V,plate1, (BX, BY))
 V= jax.lax.dynamic_update_slice(V,plate2, (BX, -BY-1))
 
-result = laplace2d_jax2_cap_plate(V, dx, dy, BX, BY, 1e-9) 
+# Compute the result and plot it.
+result = laplace2d_jax2_cap_plate(V, dx, dy, BX, BY, L1NORM_TARGET) 
 
 plot2D(x, y,result[0].T )
