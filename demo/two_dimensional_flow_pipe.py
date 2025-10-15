28e27424d2f05fdcbe6336255334beb4edbc1bac
Doc-Maxx <maxx.miller95@gmail.com>
2025-10-15 07:09:22 -0700

    started a two-dim pipeflow simulation, currently incomplete.

---
+++ demo/two_dimensional_flow_pipe.py
@@ -0,0 +1,142 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 15 05:44:35 2025
+
+@author: Doc-Maxx
+"""
+
+"""
+Here we will simulate a two dimensional flow.
+
+The flow will be incompressible. 
+
+Each velocity component will be solved using a finite difference method.
+The pressure, which maintains the incompressibility of the fluid will be solved
+each time step using a relaxational method.
+
+The calculation will proceed like this:
+    1. Set up the initial conditions and boundary conditions
+    2. Update the pressure with a relaxational solver
+    3. Update each velocity component
+    4. Enforce boundary conditions
+    5. Repeat 2-4 for every time-step
+    
+Here is depiction of the geometry of our flow:
+            Wall with no flow.
+        +-------------------------------------------------+
+        |  --->                                           |     
+        |                                                 |
+        |  --->                                           |
+Inflow  |                                                 | Outflow
+        |  --->                                           |
+        |                                                 |
+        |  --->                                           |
+        |                                                 |
+        |  --->                                           |
+        +-------------------------------------------------+
+            Wall with no flow.
+            
+We expect our steady state solution to form a parabolic velocity curve.
+Something similiar to this:
+
+        +-------------------------------------------------+
+        |  --->                         >                 |     
+        |                               ->                |
+        |  --->                         -->               |
+Inflow  |                                                 | Outflow
+        |  --->                         ----->            |
+        |                                                 |
+        |  --->                         -->               |
+        |                               ->                |
+        |  --->                         >                 |
+        +-------------------------------------------------+
+           
+"""
+
+import jax
+import jax.numpy as jnp
+import matplotlib.pyplot as plt
+import cmasher as cmr
+from tqdm import tqdm
+
+# Define some simulation parameters
+N_ITERATIONS = 30_000
+REYNOLDS_NUMBER = 100
+
+NX = 50 
+NY = 30
+DT = 0.001
+
+LENGTH = 5
+RADIUS = 1
+
+NORM_TARGET = 1e-8
+
+RHO = 1
+
+INFLOW_VELOCITY = 0.04
+PLOT_EVERY_N_STEPS = 100
+PLOT_STEP_SKIP = 1000
+
+
+def get_kinematic_viscosity(velocity, radius, reynolds_number):
+    kinematic_viscosity = velocity * 2 * radius / reynolds_number
+    return kinematic_viscosity
+
+
+def pressure_solver(pressure, horizontal_velocity, vertical_velocity, 
+                    rho, dt, dx, dy, norm_target):
+    
+    # Define loop init condition
+    init = [pressure, pressure.copy()*0, horizontal_velocity, vertical_velocity,
+            dt, dx, dy, pressure.copy()*0, norm_target]
+    
+    # First we'll compute velocity dependent part of the pressure equation
+    def get_velocity_depdent_part(horizontal_velocity, vertical_velocity, rho, dt, dx, dy, velocity_dependent_part):
+        velocity_dependent_part = velocity_dependent_part.at[1:-1,1:-1].set(
+            rho * (1 / dt * ((horizontal_velocity[1:-1,2:] - horizontal_velocity[1:-1,0:-2]) / (2 * dx) 
+                              + (vertical_velocity[2:,1:-1] - vertical_velocity[0:-2, 1:-1]) / (2 * dy)) 
+                              - ((horizontal_velocity[1:-1,2:] - horizontal_velocity[1:-1,0:-2]) / (2 * dx))**2          
+                              - 2 * ((horizontal_velocity[2:,1:-1] - horizontal_velocity[0:-2, 1:-1] / (2 * dy)
+                                      * (vertical_velocity[1:-1,2:] - vertical_velocity[1:-1,0:-2]) / (2 * dx))
+                                      - ((vertical_velocity[2:,1:-1] - vertical_velocity[0:-2, 1:-1]) / (2 * dy)) **2))
+            )
+        return velocity_dependent_part
+    
+    def get_pressure(pressure, dx, dy, velocity_dependent_part, norm_target):
+        
+    
+    # Now for the remaining portion of the pressure calculation
+    pressure_copy = pressure.copy()
+    
+    # We are setting up a JAX while loop, so we'll need a condition and body function
+    def condition(loop_state):
+        #Unpack Loop State
+        pressure = loop_state[0]
+        pressure_copy = loop_state[1]
+        l1norm_target = loop_state[-1]
+        
+        ''' 
+        Compute "norm" to compare the new voltage matrix to the previous one
+        This solver uses a relaxational method. Each step makes finer adjustments
+        to the voltage distribution. Once we reach an abritrarily small difference
+        between each step, we send the signal to terminate the loop.
+        '''
+        norm = (jnp.sum(jnp.abs(pressure[:])-jnp.abs(pressure_copy[:])) / jnp.sum(jnp.abs(pressure_copy[:])))
+        return norm > l1norm_target
+    
+    
+    def body(loop_state):
+        #unpack loop_state
+        pressure, pressure_copy, horizontal_velocity, vertical_velocity, rho, dt, dx, dy, velocity_dependent_part, norm_target = loop_state
+        # compute velocity dependent part of the pressure
+        velocity_dependent_part = get_velocity_depdent_part(horizontal_velocity,
+                                                            vertical_velocity, rho, dt, dx, dy, velocity_dependent_part)
+        # compute pressure update
+        pressure = get_pressure()
+    
+    # Loop pressure steps until pressure has relaxed to desired degree
+    result = jax.lax.while_loop(condition,
+                                body,
+                                init)
